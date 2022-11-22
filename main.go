e459e3bb626ff73df27e91931f4c6973fc3050da
tim-krebs <timkrebs9@gmail.com>
2022-11-22 20:11:02 +0100

    inital commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	//"github.com/gorilla/mux"
+	//"net/http"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	//regHandler := &registrierung.RegistrierungsHandler{}
+	//r := mux.NewRouter()
+	//r.PathPrefix("/").Methods("POST").Handler(regHandler)
+	//http.ListenAndServe(":8080", r)
+}
